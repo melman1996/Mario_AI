99842770a9921983ab6e6b27535e099a51ea676c
Damian Halatek <melman1996@gmail.com>
2020-12-01 18:32:48 +0100

    Parallelization

--- main.py
+++ main.py
@@ -6,68 +6,79 @@ import neat
 import cv2
 import pickle
 
-def eval_genomes(genomes, config):
-    for i, genome, in genomes:
-        observation = env.reset()
-        action = env.action_space.sample()
-
-        inx, iny, inc = env.observation_space.shape
-        inx = int(inx/8)
-        iny = int(iny/8)
-
-        net = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
-
-        current_max_fitness = 0
-        fitness_current = 0
-        frame = 0
-        counter = 0
-
+class Worker(object):
+    def __init__(self, genome, config):
+        self.genome = genome
+        self.config = config
+        
+    def work(self):
+        self.env = gym_super_mario_bros.make('SuperMarioBros-v0')
+        self.env = JoypadSpace(self.env, SIMPLE_MOVEMENT)
+        
+        self.env.reset()
+        
+        observation, _, _, _ = self.env.step(self.env.action_space.sample())
+        
+        inx = int(observation.shape[0]/8)
+        iny = int(observation.shape[1]/8)
         done = False
-
+        
+        net = neat.nn.FeedForwardNetwork.create(self.genome, self.config)
+        
+        fitness = 0
+        xpos = 0
+        xpos_max = 0
+        counter = 0
+        
         while not done:
-            env.render()
-            frame += 1
-
+            # self.env.render()
             observation = cv2.resize(observation, (inx, iny))
             observation = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)
             observation = np.reshape(observation, (inx, iny))
 
-            imgarray = observation.flatten()
+            imgarray = np.ndarray.flatten(observation)
+            imgarray = np.interp(imgarray, (0, 254), (-1, +1))
             nnOutput = net.activate(imgarray)
 
-            observation, reward, done, info = env.step(nnOutput.index(max(nnOutput)))
+            observation, reward, done, info = self.env.step(nnOutput.index(max(nnOutput)))
+            
+            xpos = info['x_pos']
 
-            fitness_current = info['x_pos'] + info['score']
-            #fitness_current += reward
-
+            if xpos > xpos_max:
+                xpos_max = xpos
+                counter = 0
+                fitness += 1
+            else:
+                counter += 1
+                
+            if counter > 250:
+                done = True
+                
             if info['flag_get']:
                 fitness_current += 100000
 
-            if fitness_current > current_max_fitness:
-                current_max_fitness = fitness_current
-                counter = 0
-            else:
-                counter += 1
-
-            if counter > 250: #kill mario if he is not making progress
-                done = True
-            
-            genome.fitness = fitness_current
+        return fitness
 
 
+def eval_genomes(genome, config):
+    worky = Worker(genome, config)
+    return worky.work()
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)# RIGHT_ONLY, SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
+if __name__ == '__main__':
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+                        neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                        'config-feedforward')
 
-config =  neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, 'config-feedworward')
-p = neat.Population(config)
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(10))
 
-p.add_reporter(neat.StdOutReporter(True))
-stats = neat.StatisticsReporter()
-p.add_reporter(stats)
-p.add_reporter(neat.Checkpointer(10))
+    pe = neat.ParallelEvaluator(10, eval_genomes)
 
-winner = p.run(eval_genomes)
+    winner = p.run(pe.evaluate)
 
-with open('winner.pkl', 'wb') as output:
-    pickle.dump(winner, output, 1)
+    with open('winner.pkl', 'wb') as output:
+        pickle.dump(winner, output, 1)
+
