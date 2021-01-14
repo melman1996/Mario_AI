91b93e4d0f8f02c59a899dd6b8cef59c64a445be
Damian Halatek <melman1996@gmail.com>
2021-01-14 22:38:25 +0100

    Return to previous target calculations

--- tensorflow/replay.py
+++ tensorflow/replay.py
@@ -2,13 +2,13 @@ import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from wrappers import wrapper
 from agent import Agent
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym_super_mario_bros.actions import RIGHT_ONLY
 import numpy as np
 import time
 
 if __name__ == '__main__':
     env = gym_super_mario_bros.make('SuperMarioBros-v0')
-    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    env = JoypadSpace(env, RIGHT_ONLY)
     env = wrapper(env)
 
     agent = Agent(env, max_memory=30000)
